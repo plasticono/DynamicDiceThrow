a1ab2dc525c9f39bd7b207652074b07113a56663
plasticono <plasticono@users.noreply.github.com>
2025-03-26 15:46:27 -0400

    Update MainActivity.kt

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -19,14 +19,35 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+
+    private lateinit var dieViewModel: DieViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dieViewModel = ViewModelProvider(this).get(DieViewModel::class.java)
 
         /* TODO 1: Load fragment(s)
             - Show _only_ ButtonFragment if portrait
             - show _both_ fragments if Landscape
           */
+       
+            val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+            if(isLandscape) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container1, ButtonFragment())
+                    .replace(R.id.container2, DieFragment())
+                    .commit()
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container1, ButtonFragment())
+                    .commit()
+            }
+
+
+
     }
 
     /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
@@ -36,6 +57,15 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
 
+        dieViewModel.rollDie()
+
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, DieFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
 
